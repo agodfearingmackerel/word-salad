abb6880d34c21da833dec3c5fa08f9767061e370
Sonia Tyburczy <styburczy.business@gmail.com>
2024-10-14 17:12:40 -0400

    Additional Front-End Changes (disable multiple clicking, styles Chef Joe)

--- static/main.js
+++ static/main.js
@@ -1,11 +1,17 @@
 //adds animation to the front screen -> next screen on play button click
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('play-button').addEventListener('click', function() {
+    //define button
+    const playButton = document.getElementById('play-button');
+    
+    playButton.addEventListener('click', function() {
         //get visible text
         const visibleText = document.querySelector('.visible-text');
         //get currently invisible text
         const invisibleText = document.querySelector('.invisible-text');
-
+        
+        //disable play button
+        playButton.disabled = true;
+        
         //scale down visible text
         visibleText.style.transform = 'scale(.8)';
         visibleText.style.transition = 'transform 0.2s ease';
